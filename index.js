390e3d00690a2bd5b594e7cc31495a7a85b2b345
Siam-WD <abusayedalsiam2023@gmail.com>
2023-05-04 00:41:36 +0600

    finish

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 5000;
 
+const slides = require("./data/slideShow.json");
+const chefs = require("./data/chefs.json");
 
 app.use(cors());
 
-app.get('/', (req, res)=> {
-    res.send('Flavour is cooking...')
+app.get("/", (req, res) => {
+  res.send("Flavour is cooking...");
 });
 
+app.get("/slides", (req, res) => {
+  res.send(slides);
+});
+
+app.get("/chefs", (req, res) => {
+  res.send(chefs);
+});
+
+app.get("/recipes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const selectedChef = chefs.find((c) => parseInt(c.id) === id);
+  res.send(selectedChef);
+});
 
 app.listen(port, () => {
-    console.log(`Flavour is cooking on port, ${port}`)
-})
+  console.log(`Flavour is cooking on port, ${port}`);
+});
